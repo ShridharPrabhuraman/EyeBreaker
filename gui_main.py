735653752804ff1afe917947a121bb6048bd1c81
Rudra Jikadra <rhjikadra@gmail.com>
2021-06-26 19:47:53 +0530

    Added Info tab in UI

--- gui_main.py
+++ gui_main.py
@@ -17,6 +17,37 @@ class Ui_MainWindow(object):
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
         self.tabWidget.setGeometry(QtCore.QRect(20, 10, 751, 551))
         self.tabWidget.setObjectName("tabWidget")
+        self.tab_3 = QtWidgets.QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.label_11 = QtWidgets.QLabel(self.tab_3)
+        self.label_11.setGeometry(QtCore.QRect(240, 40, 261, 71))
+        font = QtGui.QFont()
+        font.setFamily("Yrsa")
+        font.setPointSize(32)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.label_11.setFont(font)
+        self.label_11.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_11.setObjectName("label_11")
+        self.label_17 = QtWidgets.QLabel(self.tab_3)
+        self.label_17.setGeometry(QtCore.QRect(230, 110, 281, 31))
+        font = QtGui.QFont()
+        font.setFamily("aakar")
+        font.setPointSize(14)
+        font.setItalic(True)
+        self.label_17.setFont(font)
+        self.label_17.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_17.setObjectName("label_17")
+        self.label_2 = QtWidgets.QLabel(self.tab_3)
+        self.label_2.setGeometry(QtCore.QRect(20, 160, 701, 341))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName("label_2")
+        self.tabWidget.addTab(self.tab_3, "")
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
         self.label_12 = QtWidgets.QLabel(self.tab)
@@ -136,6 +167,23 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_11.setText(_translate("MainWindow", "EyeBreaker"))
+        self.label_17.setText(_translate("MainWindow", "\"Beause your eyes needs it\""))
+        self.label_2.setText(_translate("MainWindow", "In a survey by OnePoll, it was discovered that an average person spends 17 hours and 9 minutes everyday on screen, out of that around 5 hours on a computer/laptop screen time. That\'s a total of 44 years of a person\'s life that is spent watching a display. \n"
+"\n"
+"This is a serious issue that could lead to variety of eye diseases like Eye fatigue, Dry and irritated eyes, Loss of focus flexibility, Nearsightedness, Retinal damage. \n"
+"Some of these damages to a person could be permanent, others; very costly to treat. We believe that by a simple set of regular on-screen-off-screen break patterns can help rest the eyes and recover from constant eye screen damages.\n"
+"\n"
+"That is why, we are introducing EyeBreaker, a simple yet an amazingly helpful tool that will notify you to take breaks at regular interval from your computer/laptop screen so that your eyes can maintain a better long-term health. \n"
+"The tool uses your device\'s camera/webcam to track your face direction to determine if you are watching the screen or not. If you are not watching the screen or not present in front of the computer, then it considers it as your break period. \n"
+"\n"
+"Some of the customization that you can do:\n"
+" - Focus Time: How much time you can focus on-screen without taking a break.\n"
+" - Break Period: Number of seconds you want to take break.\n"
+" - Checking Period: At what intervals should the camera check if you are on-screen or off-screen.\n"
+"\n"
+"Please Note: This application is developed based on open source research and surveys. No Medical Proof has been granted by usage of this application."))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Info"))
         self.label_12.setText(_translate("MainWindow", "\"Beause your eyes needs it\""))
         self.label_10.setText(_translate("MainWindow", "EyeBreaker"))
         self.pushButton_6.setText(_translate("MainWindow", "Run"))
